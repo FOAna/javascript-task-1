cf71079707f0e00c29784982e597f8a9f0e8f04b
Ana <kristendamon@yandex.ru>
2019-10-12 00:38:19 +0500

    did js task 1

--- warmup.js
+++ warmup.js
@@ -153,7 +153,22 @@ function smilesProblem(text) {
  * @returns {'x' | 'o' | 'draw'} Результат игры
  */
 function ticTacToeProblem(field) {
-    
+    for (let i = 0; i < 3; i ++) {
+        if ((field[i][0] === field[i][1]) && (field[i][0] === field[i][2])) {
+            return field[i][0];
+        }
+        if ((field[0][i] === field[1][i]) && (field[0][i] === field[2][i])) {
+            return field[0][i];
+        }
+    }
+    if ((field[0][0] === field[1][1]) && (field[0][0] === field[2][2])) {
+        return field[0][0];
+    }
+    if ((field[0][2] === field[1][1]) && (field[0][2] === field[2][0])) {
+        return field[0][2];
+    }
+
+    return 'draw';
 }
 
 module.exports = {
